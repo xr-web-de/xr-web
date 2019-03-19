0e2b309c5a3798fd0b1151622ebbbcc7d35a9ebc
Jan Volkert Ulrich <janv.ulrich@gmail.com>
2019-03-19 17:40:15 +0100

    transparent header for start page

--- src/xr_pages/static/js/modules/siteheader.js
+++ src/xr_pages/static/js/modules/siteheader.js
@@ -3,7 +3,18 @@
 // import umbrella js
 import u from 'umbrellajs/umbrella.esm.js'
 
+const toggleHeaderBackground = () => {
+    const scrollY = Math.max(document.documentElement.scrollTop || document.body.scrollTop || window.scrollTop || 0)
+    if (scrollY > 5) {
+        u("#header").addClass('is-scrolled')
+    } else {
+        u("#header").removeClass('is-scrolled')
+    }
+}
 export const initSiteHeader = () => {
+    
+    window.addEventListener('scroll', toggleHeaderBackground)
+
     u("#nav-toggle").on('click', function() {
         u("#nav").toggleClass("open")
         u('html').toggleClass("modal-open")
