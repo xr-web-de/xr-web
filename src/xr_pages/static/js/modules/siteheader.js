89b33b5a4db587bd4966c9b36dc0672fc850d790
Jan Volkert Ulrich <janv.ulrich@gmail.com>
2019-03-19 08:13:17 +0100

    Styling for mobile main menu

--- src/xr_pages/static/js/modules/siteheader.js
+++ src/xr_pages/static/js/modules/siteheader.js
@@ -4,11 +4,7 @@
 import u from 'umbrellajs/umbrella.esm.js'
 
 export const initSiteHeader = () => {
-    console.log(u("#nav-toggle"))
-    console.log(document.getElementById('nav-toggle'))
-
     u("#nav-toggle").on('click', function() {
-        console.log('click')
         u("#nav").toggleClass("open")
     })
 }
