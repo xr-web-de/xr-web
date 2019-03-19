31847fdaffba6c96041ab13b1f8bb73563f3c66d
Jan Volkert Ulrich <janv.ulrich@gmail.com>
2019-03-19 17:23:16 +0100

    Improved mobile menu

--- src/xr_pages/static/js/modules/siteheader.js
+++ src/xr_pages/static/js/modules/siteheader.js
@@ -6,5 +6,6 @@ import u from 'umbrellajs/umbrella.esm.js'
 export const initSiteHeader = () => {
     u("#nav-toggle").on('click', function() {
         u("#nav").toggleClass("open")
+        u('html').toggleClass("modal-open")
     })
 }
