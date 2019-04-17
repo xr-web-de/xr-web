78e807590b67f189945673a3f5003414f7bc464d
xrrlz <47863033+xrrlz@users.noreply.github.com>
2019-04-17 09:43:27 +0200

    add automatic help_text to collapsiblePanel

--- src/xr_web/edit_handlers.py
+++ src/xr_web/edit_handlers.py
@@ -3,3 +3,20 @@ from wagtail.admin.edit_handlers import BaseCompositeEditHandler
 
 class FieldCollapsiblePanel(BaseCompositeEditHandler):
     template = "xr_web/edit_handlers/field_collapsible_panel.html"
+
+    def __init__(self, children=(), *args, **kwargs):
+        super().__init__(children, *args, **kwargs)
+        self.flat_children = []
+
+    def _flatten_children(self, children):
+        flat_fields = []
+        for child in children:
+            if hasattr(child, "children"):
+                flat_fields += self._flatten_children(child.children)
+            else:
+                flat_fields.append(child)
+        return flat_fields
+
+    def on_instance_bound(self):
+        super().on_instance_bound()
+        self.flat_children = self._flatten_children(self.children)
