f6dd93b86a7fc53bb7f7743abf3f9b70423bdb87
xrrlz <47863033+xrrlz@users.noreply.github.com>
2019-05-05 11:23:09 +0200

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "es6": true
+    env: {
+        node: true,
+        browser: true,
+        es6: true,
     },
-    "extends": [
-        "eslint:recommended",
-    ],
-    "parserOptions": {
-        "sourceType": "module"
+    extends: ['eslint:recommended', 'prettier'],
+    parserOptions: {
+        sourceType: 'module',
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        semi: ['error', 'never'],
+    },
+}
