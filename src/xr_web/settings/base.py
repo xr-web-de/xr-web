f165b0d356acd945ed718129f9ea3e75c3b49df0
xrrlz <47863033+xrrlz@users.noreply.github.com>
2019-05-12 11:40:49 +0200

    basic sub_menu for main_menu (level 2)

--- src/xr_web/settings/base.py
+++ src/xr_web/settings/base.py
@@ -179,9 +179,11 @@ WEBPACK_LOADER = {
 WAGTAIL_SITE_NAME = "XR de"
 
 # Wagtailmenus
-WAGTAILMENUS_DEFAULT_MAIN_MENU_TEMPLATE = "xr_pages/menus/main_menu_items.html"
+WAGTAILMENUS_DEFAULT_MAIN_MENU_TEMPLATE = "xr_pages/menus/main_menu.html"
 WAGTAILMENUS_DEFAULT_FLAT_MENU_TEMPLATE = "xr_pages/menus/flat_menu.html"
-WAGTAILMENUS_DEFAULT_CHILDREN_MENU_TEMPLATE = "xr_pages/menus/children_menu_items.html"
+WAGTAILMENUS_DEFAULT_CHILDREN_MENU_TEMPLATE = (
+    "xr_pages/menus/local_group_level_3_menu.html"
+)
 
 WAGTAILMENUS_MAIN_MENUS_MODELADMIN_CLASS = "xr_pages.wagtail_hooks.XrMainMenuAdmin"
 
