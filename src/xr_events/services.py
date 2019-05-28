d73ecc087a1b0bfa4653509dcf8a0d27a720c165
xrrlz <47863033+xrrlz@users.noreply.github.com>
2019-05-28 02:43:38 +0200

    styles and template for event_card

--- src/xr_events/services.py
+++ src/xr_events/services.py
@@ -13,13 +13,28 @@ def get_event_list_page(request=None):
     return event_list_page
 
 
-def get_event_group_pages(request=None):
+def get_event_group_pages(request=None, only_with_active_events=False):
     from .models import EventGroupPage
 
     event_list_page = get_event_list_page(request)
 
     try:
-        event_group_pages = EventGroupPage.objects.child_of(event_list_page).live()
+        event_group_pages = (
+            EventGroupPage.objects.child_of(event_list_page)
+            .live()
+            .order_by("group__name")
+        )
     except (KeyError, AttributeError, EventGroupPage.DoesNotExist):
         return []
+
+    # filter out pages with no active events
+    if only_with_active_events:
+        event_group_pages = [page for page in event_group_pages if page.upcoming_events]
+
+    # arrange regional group pages at the start
+    if any([page.is_regional_group for page in event_group_pages]):
+        regional_pages = [page for page in event_group_pages if page.is_regional_group]
+        local_pages = [page for page in event_group_pages if not page.is_regional_group]
+        event_group_pages = regional_pages + local_pages
+
     return event_group_pages
